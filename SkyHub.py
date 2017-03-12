d90bbbdc6e49007988a3ff5478be6fb4f02ae2ab
João Vitor Brandão <jvbrandaom@gmail.com>
2017-03-12 18:20:16 -0300

    Write images to disk

--- SkyHub.py
+++ SkyHub.py
@@ -8,4 +8,22 @@ def get_images_urls():
 
     return image_urls
 
-get_images_urls()
+
+def write_images_to_disk():
+    urls = get_images_urls()
+    i = 0
+    for image_url in urls:
+        image = requests.get(image_url)
+        file = open("images/image" + str(i) + ".jpg", 'wb')
+        file.write(image.content)
+        i += 1
+
+
+def main():
+    write_images_to_disk()
+
+if __name__ == '__main__':
+    main()
+
+
+
--- TestSkyHub.py
+++ TestSkyHub.py
@@ -1,11 +1,17 @@
 import unittest
+import os
 from SkyHub import get_images_urls
+from SkyHub import write_images_to_disk
 
 
 class TestSkyHub(unittest.TestCase):
     def test_image_urls_amount(self):
         images_urls= get_images_urls()
         self.assertEqual(len(images_urls), 10)
+
+    def test_write_images_to_disk(self):
+        write_images_to_disk()
+        self.assertEqual(len(os.listdir("images")), 10)
 
 if __name__ == '__main__':
     unittest.main()
