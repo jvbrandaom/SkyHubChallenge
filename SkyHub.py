64cc14551d89340085aac8182926331aa73b1f2e
João Vitor Brandão <jvbrandaom@gmail.com>
2017-03-12 22:22:02 -0300

    Creates methods to write images to db and resize images

--- SkyHub.py
+++ SkyHub.py
@@ -1,4 +1,13 @@
 import requests
+import os
+from PIL import Image
+import gridfs
+
+small = 320, 320
+medium = 384, 288
+large = 640, 480
+image_folder = "images/"
+
 
 
 def get_images_urls():
@@ -14,13 +23,36 @@ def write_images_to_disk():
     i = 0
     for image_url in urls:
         image = requests.get(image_url)
-        file = open("images/image" + str(i) + ".jpg", 'wb')
+        file = open(image_folder + "image" + str(i) + ".jpg", 'wb')
         file.write(image.content)
         i += 1
 
 
+def write_images_to_db(db):
+    urls = get_images_urls()
+    fs = gridfs.GridFS(db)
+    i = 0
+    for image_url in urls:
+        image = requests.get(image_url, stream=True)
+        fs.put(image.raw, filename="image" + str(i) + ".jpg", contentType="image/jpeg")
+        i += 1
+
+
+def resize_images():
+    images = os.listdir(image_folder)
+    for image_name in images:
+        image = Image.open(image_folder + image_name)
+        image.thumbnail(small)
+        splitted_image_name = image_name.split(".")
+        image.save(image_folder + splitted_image_name[0] + "_small." + splitted_image_name[1])
+
+
+
 def main():
+
+
     write_images_to_disk()
+    resize_images()
 
 if __name__ == '__main__':
     main()
